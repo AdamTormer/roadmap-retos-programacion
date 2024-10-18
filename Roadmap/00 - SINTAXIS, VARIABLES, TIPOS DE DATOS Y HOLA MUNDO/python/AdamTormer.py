04eca40b553a95792f60b38bcbf8eacf8c845434
AdamTormer <watanuki.nadesco@gmail.com>
2024-10-18 02:04:30 -0500

    Update AdamTormer.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AdamTormer.py
@@ -0,0 +1,12 @@
+# https://www.python.org/
+
+# Mi comentario (python es genial) para colocar una comentario en un linea usas (#) esto sirve para que puedas agregar info en una linea
+
+"""Mi comentario varias lineas 
+Con las 3 comillas se puede agregar comentarios en varias lineas.
+Al principo y al final de debes colocarla para que identifique que es un comentario, diferente del (#)
+donde no hay que hacer una apertura y un cierre 
+phyton es genial.
+"""
+
+mi_variable = "esta no tiene valor al menos q se asigne o sea algun resultado de una ecuacion"
